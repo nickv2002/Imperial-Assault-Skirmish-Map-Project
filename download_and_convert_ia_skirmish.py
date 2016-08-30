1dc2802298906dc9ba947e5268d5f3a3adcf0ae8
Nick Vance <nick@towatchlist.com>
2016-08-30 13:17:48 -0700

    new border between maps for cutouts

--- download_and_convert_ia_skirmish.py
+++ download_and_convert_ia_skirmish.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# coding=utf-8
+
 # Download IA maps and convert them to PDFs for printing
 
 import sys, os, urllib
@@ -13,19 +15,20 @@ totalNumberOfMaps = 32
 
 mapNumbers = range(1, totalNumberOfMaps+1)
 
-# for mapNumber in mapNumbers:
-#     fileURL = 'http://ibrahimshaath.co.uk/imperialassault/%02i.jpg' % mapNumber
-#     print 'Downloading:', fileURL
-#     urllib.urlretrieve(fileURL, os.path.split(fileURL)[1])
+for mapNumber in mapNumbers:
+    continue # already downloaded them for testing
+    fileURL = 'http://ibrahimshaath.co.uk/imperialassault/%02i.jpg' % mapNumber
+    print 'Downloading:', fileURL
+    urllib.urlretrieve(fileURL, os.path.split(fileURL)[1])
 
 fileNumberIter = 1
 
 print "\nDownloads done. Combining Images. This might take some processing time..."
 
-for listOfMapNumsToCombine in chunks(mapNumbers, 6):
+for listOfMapNumsToCombine in chunks(mapNumbers, 8):
     jpgFile = 'map_group_%i.jpg' % fileNumberIter
     # For tournament maps use this name instead:
-    # jpgFile = 'tournament_maps_%i.jpg' % fileNumberIter
+    jpgFile = 'tournament_maps_%i.jpg' % fileNumberIter
 
     # For tournament maps just print these map numbers
     # if you want to make a custom selection of maps, you can enter them here too
@@ -38,18 +41,16 @@ for listOfMapNumsToCombine in chunks(mapNumbers, 6):
     # combine images:
     images = map(Image.open, sourceList)
     widths, heights = zip(*(i.size for i in images))
+    total_width   = sum(widths)
+    max_height    = max(heights)
 
-    total_width = sum(widths)
-    max_height  = max(heights)
-
-    # add 12 pixel border (24 pix on each dimension) to all sides for cutoff area
-    # luckily, this is already black!
-    combined_im = Image.new('RGB', (total_width + 24 , max_height + 24))
-
-    x_offset = 12
+    # add 2 pixels of white between each map to help with cutting them apart
+    combined_size = (total_width - 2 + len(listOfMapNumsToCombine) * 2, max_height)
+    combined_im = Image.new('RGB', combined_size, color = (255,255,255))
+    x_offset = 0
     for im in images:
-        combined_im.paste(im, (x_offset, 12))
-        x_offset += im.size[0]
+        combined_im.paste(im, (x_offset, 0))
+        x_offset += im.size[0] + 2 #add 2 pixels for division
 
     # Save new image as a JPG
     combined_im.save( jpgFile, dpi=[300,300] )
@@ -59,4 +60,4 @@ for listOfMapNumsToCombine in chunks(mapNumbers, 6):
     fileNumberIter += 1
 
     # If you just want to print one page of stuff, eg for tournament maps, break here and don't loop
-    # break
+    break
